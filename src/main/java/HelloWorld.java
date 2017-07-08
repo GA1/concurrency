5cd62c702c56990e781575d8eb7e68814bcd5660
GA1 <nvkga1@gmail.com>
2017-07-08 12:37:03 +0200

    Clean

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,13 +2,18 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        final Object lock = new Object();
         Thread t1 = new Thread(new Runnable() {
             public void run() {
                 System.out.println("Hello World");
             }
         });
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                System.out.println("Hello World");
+            }
+        });
         t1.run();
+        t2.run();
 
     }
 
